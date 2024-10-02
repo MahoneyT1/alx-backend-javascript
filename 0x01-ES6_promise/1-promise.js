0eb711d2e41e3d53eeea80c8c99a92a17042db63
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-02 20:14:17 +0100

    handling promises error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,13 +3,13 @@
 // Using the prototype below, return a promise. The parameter is a boolean.
 
 export default function getFullResponseFromAPI(success) {
-    const getpromiseResponse = new Promise((resolve, reject) => {
-      if (success === true) {
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  
-    return getpromiseResponse;
-  }
+  const getpromiseResponse = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+
+  return getpromiseResponse;
+}
