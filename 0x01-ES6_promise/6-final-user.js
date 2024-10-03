274bc90be4fbe9703e0c61772081b40ccd027299
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-03 15:46:27 +0100

    sing async function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,3 @@
-// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
-
-// Write and export a function named handleProfileSignup. It should accept three arguments
-// firstName (string), lastName (string), and fileName (string). The function should call
-// the two other functions. When the promises are all settled it should return an array with
-// the following structure:
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
