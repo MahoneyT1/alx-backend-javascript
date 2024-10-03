9c068026ff5f88d18a81381930e01e7d07acbae4
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-03 06:44:28 +0100

    rejecting promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,11 +1,13 @@
-// Write and export a function named uploadPhoto. It should accept one argument fileName (string).
+// Write and export a function named uploadPhoto. It should
+// accept one argument fileName (string).
 
-// The function should return a Promise rejecting with an Error and the string $fileName cannot be processed
+// The function should return a Promise rejecting with an
+// Error and the string $fileName cannot be processed
 
 // export default function uploadPhoto(filename) {
 
 // }
 
 export default function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} cannot be processed`))
-};
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
