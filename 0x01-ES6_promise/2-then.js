da0710f13fd4224e552b5989e57911f3a9c5fdc3
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-03 05:59:13 +0100

    created new promise and returned resolved

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,3 @@
-// Append three handlers to the function:
-
-// When the Promise resolves, return an object with the following attributes
-// status: 200
-// body: success
-// When the Promise rejects, return an empty Error object
-// For every resolution, log Got a response from the API to the console
-
 export default function handleResponseFromAPI(promise) {
   const obj = { status: 200, body: 'Status' };
 
