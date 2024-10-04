9cb0a37eac8ab6d228b1b5415c8da51cabe8b93c
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-04 09:09:01 +0100

    fixed the then method

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -11,11 +11,11 @@
 
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((resolve) => {
-      resolve({
+    .then(() => {
+      return {
         status: 200,
         body: 'success',
-      });
+      };
     })
     .catch(() => new Error(''))
     .finally(() => {
