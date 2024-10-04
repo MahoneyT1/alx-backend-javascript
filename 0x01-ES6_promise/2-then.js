171c0f5a7562f2f37d6075065018e90707f5ee73
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-04 08:50:43 +0100

    fixed the then method

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,22 @@
+// Using the function prototype below
+
+// function handleResponseFromAPI(promise)
+// Append three handlers to the function:
+
+// When the Promise resolves, return an object with the following attributes
+// status: 200
+// body: success
+// When the Promise rejects, return an empty Error object
+// For every resolution, log Got a response from the API to the console
+
 export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'status' };
-
   return promise
-    .then(() => obj)
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
-
-};
-return obj
+    .then(() => ({
+      status: 200,
+      body: 'sucess',
+    }))
+    .catch(() => new Error(''))
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
