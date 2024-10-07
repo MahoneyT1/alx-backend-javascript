f98a9c5c54d62925c95e49a3700deabfe21ed271
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-07 13:06:00 +0100

    return a list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,25 @@
+export default function getListStudents() {
+  const allStudent = [];
+
+  const firstStudent = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const secondStudent = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const thirdStudent = {
+    id: 5,
+    firstName: 'serena',
+    location: 'San Francisco',
+  };
+
+  allStudent.push(firstStudent, secondStudent, thirdStudent);
+
+  return allStudent;
+}
