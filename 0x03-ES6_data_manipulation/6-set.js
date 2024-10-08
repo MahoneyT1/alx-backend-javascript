4dbaae745a9f001f891341b8a2ae6a6bae9d9f24
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-08 12:41:29 +0100

    using set to filter out double data

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+// Create a function named setFromArray that returns a Set from an array.
+
+// It accepts an argument (Array, of any kind of element).
+
+export default function setFromArray(arr) {
+  const newFilteredArray = new Set(arr);
+
+  return [...newFilteredArray];
+}
