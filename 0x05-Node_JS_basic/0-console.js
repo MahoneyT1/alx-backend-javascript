2e0198570559afe2ddfb49a31e1a76bcbcd4bd2c
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-25 14:23:39 +0100

    process.stdout

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * create a function named displayMessage that prints in STDOUT the string argument.
+ */
+
+const process = require('process');
+
+const displayMessage = (str) => {
+  process.stdout.write(`${str}\n`);
+};
+
+module.exports = displayMessage;
