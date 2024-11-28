e3735bc5409c91734d9f179bd3dde09beb9f5914
MahoneyT1 <ifeanyitech15@gmail.com>
2024-11-28 02:05:48 +0100

    debugging

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,20 @@
+/**
+*Create a program named 1-stdin.js that will be executed through command line:
+
+It should display the message Welcome to Holberton School,
+what is your name? (followed by a new line)
+The user should be able to input their name on a new line
+The program should display Your name is: INPUT
+When the user ends the program, it should display This important
+software is now closing (followed by a new line)
+*/
+
+// Display the welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()} \n`);
     process.exit();
   });
 } else {
@@ -11,6 +23,6 @@ if (process.stdin.isTTY) {
     process.exit();
   });
   process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+    process.stdout.write('This important software is now closing \n');
   });
 }
