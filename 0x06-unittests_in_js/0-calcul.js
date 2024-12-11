1623d3667a03ef8684711c022f9abf81c90fb6c6
MahoneyT1 <ifeanyitech15@gmail.com>
2024-12-11 19:55:41 +0100

    commit files

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function calculateNumber(a, b){
+    const result = math.round(a + b);
+    return result;
+};
+
+
---
+++ 0x06-unittests_in_js/tests/0-calcul.test.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const 
+
+// test cases for the function add
