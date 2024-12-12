e07595eda6910277c7a7a816298c414ffccd142c
MahoneyT1 <ifeanyitech15@gmail.com>
2024-12-12 23:15:50 +0000

    test cases

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,9 @@
 
-export default function calculateNumber(a, b){
+function calculateNumber(a, b){
     const argsA = Math.round(a);
     const argsB = Math.round(b);
 
     return argsA + argsB;
 };
+
+module.exports = calculateNumber;
